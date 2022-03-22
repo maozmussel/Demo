25f467cae92902fe48c40b2d51e62c07089efeb2
Maoz Mussel <Maoz.Mussel@getwizer.com>
2022-03-22 14:50:56 +0200

    Changed schema

--- myworkspace/admin.py
+++ myworkspace/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 
-from .models import User, Role, Project
+from .models import Project
 
-admin.site.register(User)
-admin.site.register(Role)
 admin.site.register(Project)
--- myworkspace/models.py
+++ myworkspace/models.py
@@ -1,28 +1,11 @@
 from django.db import models
 
-class Role(models.Model):
-    role_id = models.IntegerField(primary_key=True)
-    role_name = models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.role_name
-
-class User(models.Model):
-    username = models.CharField(max_length=20, primary_key=True)
-    password = models.CharField(max_length=20)
-    full_name = models.CharField(max_length=50)
-    role = models.ForeignKey(Role, on_delete=models.PROTECT)
-
-    def __str__(self):
-        return self.username
-
 class Project(models.Model):
-    project_id = models.IntegerField(default=0)
     project_name = models.CharField(max_length=50)
     created_date = models.DateTimeField('date created')
-    owner = models.ForeignKey(User, on_delete=models.PROTECT)
     survey_id = models.IntegerField(default=0)
     survey_url = models.CharField(max_length=200, null=True)
+    survey_type = models.CharField(max_length=30, default='General')
 
     def __str__(self):
         return self.project_name
